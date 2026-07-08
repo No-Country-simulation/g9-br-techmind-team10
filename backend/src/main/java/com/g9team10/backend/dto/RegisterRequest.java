7ff5d8e5d7d8b106d2f82ab5f386a7f9a6fe2332
Gui11epio <guilherme.laiber@gmail.com>
2026-07-08 15:17:55 -0300

    feat: Criação do Cadastro/Login de usuários

---
+++ backend/src/main/java/com/g9team10/backend/dto/RegisterRequest.java
@@ -0,0 +1,6 @@
+package com.g9team10.backend.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record RegisterRequest(@NotBlank String username, @NotBlank String email, @NotBlank String password) {
+}
