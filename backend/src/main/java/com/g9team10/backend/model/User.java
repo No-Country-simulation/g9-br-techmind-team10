da6ac8d003128511ec3615ddc6ef8e36a408252f
Gui11epio <guilherme.laiber@gmail.com>
2026-07-13 17:36:07 -0300

    Branch atualizada

--- backend/src/main/java/com/g9team10/backend/model/User.java
+++ backend/src/main/java/com/g9team10/backend/model/User.java
@@ -1,9 +1,7 @@
 package com.g9team10.backend.model;
-
 import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.Collection;
 import java.util.List;
 
